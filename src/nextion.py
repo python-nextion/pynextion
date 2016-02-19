52d3bfa44818e5115bf381c4b500c81ddf174910
Raffaele Montella <raffaele.montella@uniparthenope.it>
2016-02-19 07:00:59 -0600

    Page Controls Text classes added

--- src/nextion.py
+++ src/nextion.py
@@ -3,7 +3,40 @@ import sys
 import time
 from threading import Thread
 
+class Page(object):
+  def __init__(self, nextion,id):
+    self.nextion=nextion
+    self.id=id
+    self.controls=[]
 
+  def hookText(self,id,value=None):
+    control=Text(self,id,value)
+    self.controls.append(control)
+    return control
+
+  def show(self):
+    self.nextion.setPage(self.id)
+
+
+class Control(object):
+
+  def __init__(self,page,id):
+    self.page=page
+    self.id=id
+
+class Text(Control):
+
+  def __init__(self,page,id,value=None):
+    super(Text, self).__init__(page,id)
+    if value is not None:
+      self.page.nextion.setText(self.id,value)
+
+  def get(self):
+    return self.page.nextion.getText(self.id)
+
+  def set(self,value):
+    self.page.nextion.setText(self.id,value)
+    
 class Nextion(object):
 
   ERRORS={
@@ -15,11 +48,25 @@ class Nextion(object):
     "11": "Baud rate setting invalid",
     "12": "Curve control ID number or channel number is invalid",
     "1a": "Variable name invalid",
-    "1b": "Variable operation invalid",
+    "1b": "Variable operation invalid"
+  }
+
+  MESSAGES={
+    "65": "Touch event return data",
     "66": "Current page ID number returns"
   }
 
+  RED   =63488	
+  BLUE  =31	
+  GRAY  =33840	
+  BLACK =0	
+  WHITE =65535	
+  GREEN =2016	
+  BROWN =48192
+  YELLOW=65504
+
   def __init__(self,ser):
+    self.pages = []
     self.debug = False
     self.ser = ser
     while True:
@@ -29,6 +76,11 @@ class Nextion(object):
       except:
         print "Wait..."
         time.sleep(1)
+
+  def hookPage(self,id):
+    page=Page(self,id)
+    self.pages.append(page)
+    return page
 
   def setDebug(self,debug):
     self.debug=debug
@@ -56,8 +108,27 @@ class Nextion(object):
 
     raise ValueError(Nextion.getErrorMessage(0x00))
 
+###############################
+
+  def refresh(self,id="0"):
+    s=self.nxWrite('ref %s' % id)
+    if s[0]!=0x01:
+      raise ValueError(Nextion.getErrorMessage(s[0]))
+
+  def getText(self,id):
+    s=self.nxWrite('get %s.txt' % id)
+    if s[0]!=0x01:
+      raise ValueError(Nextion.getErrorMessage(s[0]))
+
+  def getValue(self,id):
+    s=self.nxWrite('get %s.val' % id)
+    if s[0]!=0x01:
+      raise ValueError(Nextion.getErrorMessage(s[0]))
+
+###############################
+
   def setText(self,id,value):
-    s=self.nxWrite(id+'.txt="'+value+'"')
+    s=self.nxWrite(id+'.txt="'+str(value)+'"')
     if s[0]!=0x01:
       raise ValueError(Nextion.getErrorMessage(s[0])+": id: "+id+" text:"+ value)
 
@@ -74,7 +145,7 @@ class Nextion(object):
     if s[0]!=0x01:
       raise ValueError(Nextion.getErrorMessage(s[0]))
 
-  def drawText(self,x1,y1,x2,y2,fontid,fontcolor,backcolor,xcenter,ycenter,sta,string):
+  def drawString(self,x1,y1,x2,y2,fontid,fontcolor,backcolor,xcenter,ycenter,sta,string):
     s=self.nxWrite('xstr %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s' % (x1,y1,x2-x1,y2-y1,fontid,fontcolor,backcolor,xcenter,ycenter,sta,string))
     if s[0]!=0x01:
       raise ValueError(Nextion.getErrorMessage(s[0]))
@@ -162,15 +233,41 @@ if __name__ == "__main__":
   ser.flushOutput()
   nextion=Nextion(ser)
   print 'Serial connected'
-  nextion.setDim(50)
-  for p in range(0,3):
-    nextion.setPage(p)
-    nextion.setText('t0',"Fede<3")
-    print nextion.getPage()
-  nextion.setDim(100)
-  nextion.clear(64)
-  nextion.drawBox(0,0,100,100,256)
-  nextion.drawRectangle(0,0,100,100,512)
-  nextion.drawLine(0,0,100,100,1024)
-  nextion.drawCircle(100,100,50,2048)
-  #nextion.drawText(0,0,400,200,0,4096,3072,1,1,1,"*")
+  #nextion.setDim(50)
+  #for p in range(0,3):
+  #  nextion.setPage(p)
+  #  nextion.setText('t0',"Fede<3")
+  #  print nextion.getPage()
+  #nextion.setDim(100)
+
+  pageBoatSpeed=nextion.hookPage(0)
+  pageHeading=nextion.hookPage(1)
+  pageWindSpeed=nextion.hookPage(2)
+
+  txtBoatSpeedValue=pageBoatSpeed.hookText("t1")
+  txtBoatSpeedAttr=pageBoatSpeed.hookText("t2")
+  txtBoatSpeedUnit=pageBoatSpeed.hookText("t3")
+
+  txtHeadingValue=pageBoatSpeed.hookText("t1")
+  txtHeadingTrueMag=pageBoatSpeed.hookText("t3")
+
+  txtWindSpeedValue=pageWindSpeed.hookText("t1")
+  txtWindSpeedTrueApp=pageWindSpeed.hookText("t2")
+  txtWindSpeedUnit=pageWindSpeed.hookText("t3")
+
+  pageBoatSpeed.show()
+  txtBoatSpeedValue.set(2.2)
+  pageHeading.show()
+  txtHeadingValue.set(300)
+  pageWindSpeed.show()
+  txtWindSpeedValue.set(3.75)
+
+  nextion.clear(Nextion.RED)
+  nextion.drawBox(0,0,100,100,Nextion.BLUE)
+  nextion.drawRectangle(0,0,100,100,Nextion.YELLOW)
+  nextion.drawLine(0,0,100,100,Nextion.GREEN)
+  nextion.drawCircle(100,100,50,Nextion.BROWN)
+  #nextion.drawString(0,0,400,200,2,4096,3072,1,1,1,"*")
+
+  pageBoatSpeed.show()
+  txtBoatSpeedValue.set("Fede<3")
